a5ca937a7a5c3eabf85c5f1d74f28bfb1a722aa9
smileMrLee <wnlirong@163.com>
2016-03-27 10:13:33 +0800

    优化gson字符串类

---
+++ fans-bs-web/src/main/java/com/eden/fans/bs/web/controller/UploadController.java
@@ -0,0 +1,15 @@
+package com.eden.fans.bs.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2016/3/27.
+ * 上传文件
+ */
+
+@Controller
+@RequestMapping(value = "/common")
+public class UploadController {
+
+}
