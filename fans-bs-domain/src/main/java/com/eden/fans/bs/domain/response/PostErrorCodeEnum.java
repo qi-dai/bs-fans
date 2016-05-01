7e449b13b0c33b6d04b6968aae50cfb558be0ee0
smileMrLee <wnlirong@163.com>
2016-05-01 23:50:24 +0800

    统一成功code码

--- fans-bs-domain/src/main/java/com/eden/fans/bs/domain/response/PostErrorCodeEnum.java
+++ fans-bs-domain/src/main/java/com/eden/fans/bs/domain/response/PostErrorCodeEnum.java
@@ -5,23 +5,23 @@ package com.eden.fans.bs.domain.response;
  */
 public enum PostErrorCodeEnum implements BaseCodeEnum {
     SUCCESS("0", "成功", "成功"),
-    CREATE_POST_SUCCESS("3001", "创建帖子成功", "创建帖子成功"),
+    CREATE_POST_SUCCESS("0", "创建帖子成功", "创建帖子成功"),
     CREATE_POST_FAILED("-3001", "创建帖子失败", "创建帖子失败，请稍后再试"),
     CREATE_POST_AUTH_FAILED("-30011", "创建帖子失败", "只有管理员可以创建广告帖子"),
-    PRAISE_POST_SUCCESS("3002", "点赞成功", "点赞成功"),
+    PRAISE_POST_SUCCESS("0", "点赞成功", "点赞成功"),
     PRAISE_POST_FAILED("-3002", "点赞失败", "点赞失败，请稍后再试"),
-    CONCERN_POST_SUCCESS("3003", "关注成功", "关注成功"),
+    CONCERN_POST_SUCCESS("0", "关注成功", "关注成功"),
     CONCERN_POST_FAILED("-3003", "关注失败", "关注失败，请稍后再试"),
-    REPLY_POST_SUCCESS("3004", "回帖成功", "回帖成功"),
+    REPLY_POST_SUCCESS("0", "回帖成功", "回帖成功"),
     REPLY_POST_FAILED("-3004", "回帖失败", "回帖失败，请稍后再试"),
-    UPDATE_POST_SUCCESS("3005", "更新帖子状态成功", "更新帖子状态成功"),
+    UPDATE_POST_SUCCESS("0", "更新帖子状态成功", "更新帖子状态成功"),
     UPDATE_POST_FAILED("-3005", "更新帖子状态失败", "更新帖子状态失败,请稍后再试"),
     UPDATE_POST_AUTH_FAILED("-30051", "更新帖子状态失败", "更新帖子状态失败，您不是帖子的创建人"),
-    APPROVE_POST_SUCCESS("3007", "审批操作成功", "审批操作成功"),
+    APPROVE_POST_SUCCESS("0", "审批操作成功", "审批操作成功"),
     APPROVE_POST_FAILED("-3007", "审批操作失败", "审批操作失败失败,请稍后再试"),
-    GET_APPROVAL_POST_SUCCESS("3006", "获取待审批的帖子成功", "获取待审批的帖子成功"),
+    GET_APPROVAL_POST_SUCCESS("0", "获取待审批的帖子成功", "获取待审批的帖子成功"),
     GET_APPROVAL_POST_FAILED("-3006", "获取待审批的帖子失败", "获取待审批的帖子失败，您不是管理员"),
-    GET_APPROVED_POST_SUCCESS("3008", "获取已审批的帖子成功", "获取已审批的帖子成功"),
+    GET_APPROVED_POST_SUCCESS("0", "获取已审批的帖子成功", "获取已审批的帖子成功"),
     GET_APPROVED_POST_FAILED("-3008", "获取已审批的帖子失败", "获取待审批的帖子失败，您不是管理员");
     //RPC调用异常
     public String code;
