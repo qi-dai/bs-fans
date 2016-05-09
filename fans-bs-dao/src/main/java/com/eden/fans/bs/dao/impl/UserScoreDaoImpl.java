7e82cbb33a9a94f978ddc4e1ef37754a0aa17bcf
smileMrLee <wnlirong@163.com>
2016-05-09 21:00:05 +0800

    积分代码提交

--- fans-bs-dao/src/main/java/com/eden/fans/bs/dao/impl/UserScoreDaoImpl.java
+++ fans-bs-dao/src/main/java/com/eden/fans/bs/dao/impl/UserScoreDaoImpl.java
@@ -3,10 +3,12 @@ package com.eden.fans.bs.dao.impl;
 import com.eden.fans.bs.dao.BaseDao;
 import com.eden.fans.bs.dao.IUserScoreDao;
 import com.eden.fans.bs.domain.user.UserScoreVo;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Administrator on 2016/5/7.
  */
+@Repository
 public class UserScoreDaoImpl extends BaseDao<UserScoreVo> implements IUserScoreDao {
     @Override
     public boolean addUserScore(UserScoreVo userScoreVo) {
